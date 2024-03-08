19690e9d392cf7deeadb07eb43d722fa937431b2
katerinazakova <zakova.kz@gmail.com>
2024-03-08 09:31:54 +0100

    feat: establish project drink_water

---
+++ drink_water/src/main/java/com/github/katerinazakova/drinkWater/DrinkWaterApplication.java
@@ -0,0 +1,13 @@
+package com.github.katerinazakova.drinkWater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkWaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkWaterApplication.class, args);
+	}
+
+}
---
+++ drink_water/src/test/java/com/github/katerinazakova/drinkWater/DrinkWaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.katerinazakova.drinkWater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkWaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
